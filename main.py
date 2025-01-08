82b18d91e7222e7b17d6b5bcefde799611b0f0b8
MikaWes81 <147302460+MikaWes81@users.noreply.github.com>
2025-01-08 16:30:38 +0000

    hinzu cordinaten und erklärung

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+clear = lambda: os.system('clear')
+clear()
+
+class bcolors:
+    blue = '\33[34m'
+    end = '\033[0m'
 
 
-
-
+cord = [0,0]
 
 player_input_blue  = ["0","0","0","0","0","0","0","0","0","0",
                       "0","0","0","0","0","0","0","0","0","0",
@@ -144,27 +150,61 @@ def print_board_player_red_input():
                 number = number + 1
 
 
-#def setup_board():
+#def set_player_blue
+
+
+
+def place_ship():
+    cordinats = input
+    cord = cordinats.split(",")
+    if cordinats == "":
+        return 0
+    else:
+        return 1
+
+
+# 0 not correct
+
+def setup_board():
+    print(bcolors.blau + "Blau" + bcolors.end )
+    print("Presse enter um fortzufahren")
+    input()
+    clear()
     
 
 
+setup_board()
 
 
-
-def setup():
-    for rang in range(100):
-        player_red[rang]         = 0
-        player_blue[rang]        = 0
-        player_input_blue[rang]  = 0
-        player_input_red[rang]   = 0
+def info():
+    print("die eingaben werden über Kordinaten gemacht")
+    print("sie werden x,y geschrieben")
+    print("x ist die Horizontale cordinate")
+    print("y ist die Verticale cordinate")
+    print("presse Enter um fortzufahren")
+    input()
+    print("wärend der phase wo die schiffen gesetzt werden")
+    print("darf nur der aktive spieler das gerät und den bildschirm benutzen")
+    print("presse Enter um fortzufahren")
+    input()
     return
 
 
+#def setup():
+#    for rang in range(100):
+#        player_red[rang]         = 0
+#        player_blue[rang]        = 0
+#        player_input_blue[rang]  = 0
+#        player_input_red[rang]   = 0
+#    return
 
 
-def main():
-    setup()
-    setup_board()
-    #play()
 
-print_board()
+
+#def main():
+#    setup()
+#    info()
+#    setup_board()
+#    play()
+
+#*/
