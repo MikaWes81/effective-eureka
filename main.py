f59bf2118a4144c0822362b1b9f8d637744d5254
MikaWes81 <mika.weske@protonmail.com>
2025-01-17 21:23:35 +0100

    update local

--- main.py
+++ main.py
@@ -1,90 +1,96 @@
 import os
+
 clear = lambda: os.system('clear')
 clear()
+
 
 class bcolors:
     blue = '\33[34m'
     end = '\033[0m'
 
+
 placed_blue = 0
 placed_red = 0
 
 #                     +1  +2  +3  +4  +5  +6  +7  +8  +9  +10
-player_input_blue  = ["0","0","0","0","0","0","0","0","0","0",  # +0
-                      "0","0","0","0","0","0","0","0","0","0",  # +10
-                      "0","0","0","0","0","0","0","0","0","0",  # +20
-                      "0","0","0","0","0","0","0","0","0","0",  # +30
-                      "0","0","0","0","0","0","0","0","0","0",  # +40
-                      "0","0","0","0","0","0","0","0","0","0",  # +50
-                      "0","0","0","0","0","0","0","0","0","0",  # +60
-                      "0","0","0","0","0","0","0","0","0","0",  # +70
-                      "0","0","0","0","0","0","0","0","0","0",  # +80
-                      "0","0","0","0","0","0","0","0","0","0",] # +90
-    
-player_blue =        ["0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",
-                      "0","0","0","0","0","0","0","0","0","0",]
-    
-    
-player_input_red  = ["0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",
-                     "0","0","0","0","0","0","0","0","0","0",]
+player_input_blue = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +0
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +10
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +20
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +30
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +40
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +50
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +60
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +70
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",  # +80
+                     "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", ]  # +90
 
-player_red     =  ["0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",
-                   "0","0","0","0","0","0","0","0","0","0",]
+player_blue = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+               "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", ]
+
+player_input_red = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+                    "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", ]
+
+player_red = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0",
+              "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", ]
+
 
 def print_board_red():
     line = 0
-    
-    höhe = 1       
+
+    höhe = 1
     print("  1 2 3 4 5 6 7 8 9 10")
     for i in range(10):
-        print(str(höhe) + " " + player_red[line]+ " "+ player_red[line+1]+" "+player_red[line+2]+" "+player_red[line+3]+" "+player_red[line+4]+" "+player_red[line+5]+" "+player_red[line+6]+" "+player_red[line+7]+" "+player_red[line+8]+" "+player_red[line+9])
+        print(str(höhe) + " " + player_red[line] + " " + player_red[line + 1] + " " + player_red[line + 2] + " " +
+              player_red[line + 3] + " " + player_red[line + 4] + " " + player_red[line + 5] + " " + player_red[
+                  line + 6] + " " + player_red[line + 7] + " " + player_red[line + 8] + " " + player_red[line + 9])
         höhe += 1
         line += 10
+
+
 def print_bord_blue():
     line = 0
-    
-    höhe = 1        
+
+    höhe = 1
     print("   1 2 3 4 5 6 7 8 9 10")
     for i in range(9):
-        print(str(höhe) + "  " + player_blue[line]+ " "+ player_blue[line+1]+" "+player_blue[line+2]+" "+player_blue[line+3]+" "+player_blue[line+4]+" "+player_blue[line+5]+" "+player_blue[line+6]+" "+player_blue[line+7]+" "+player_blue[line+8]+" "+player_blue[line+9])
+        print(str(höhe) + "  " + player_blue[line] + " " + player_blue[line + 1] + " " + player_blue[line + 2] + " " +
+              player_blue[line + 3] + " " + player_blue[line + 4] + " " + player_blue[line + 5] + " " + player_blue[
+                  line + 6] + " " + player_blue[line + 7] + " " + player_blue[line + 8] + " " + player_blue[line + 9])
         höhe += 1
         line += 10
-    print(str(höhe) + " " + player_blue[line]+ " "+ player_blue[line+1]+" "+player_blue[line+2]+" "+player_blue[line+3]+" "+player_blue[line+4]+" "+player_blue[line+5]+" "+player_blue[line+6]+" "+player_blue[line+7]+" "+player_blue[line+8]+" "+player_blue[line+9])
-
-#def set_player_blue
-
-
+    print(str(höhe) + " " + player_blue[line] + " " + player_blue[line + 1] + " " + player_blue[line + 2] + " " +
+          player_blue[line + 3] + " " + player_blue[line + 4] + " " + player_blue[line + 5] + " " + player_blue[
+              line + 6] + " " + player_blue[line + 7] + " " + player_blue[line + 8] + " " + player_blue[line + 9])
 
 
+# def set_player_blue
 
 
 # 0 not correct
-
-
 
 
 def boot_blue():
@@ -96,15 +102,14 @@ def boot_blue():
     print("Endpunkt")
     cordinats2 = input()
     cord = cordinats.split(",")
-    cord2= cordinats2.split(",")
+    cord2 = cordinats2.split(",")
 
-
-    if int(cord[0]) - int(cord2[0]) == 0 and int(cord[1]) - int(cord2[1]) !=0:
-        ent =abs(int(cord[1]) + int(cord2[1]))
+    if int(cord[0]) - int(cord2[0]) == 0 and int(cord[1]) - int(cord2[1]) != 0:
+        ent = abs(int(cord[1]) + int(cord2[1]))
         if cord[1] > cord2[1]:
-            punkt = int(cord2[1])*10 -10
-        elif cord2[1]> cord[1]:
-            punkt = int(cord[1])*10 -10
+            punkt = int(cord2[1]) * 10 - 10
+        elif cord2[1] > cord[1]:
+            punkt = int(cord[1]) * 10 - 10
         if ent > 5:
             print("prese Enter un fortzufahren")
             print("ungültige eingabe")
@@ -117,27 +122,27 @@ def boot_blue():
             placed_blue = placed_blue + 1
             for _ in range(1):
                 punkt += 1
-                player_blue[punkt+int(cord[0]) -2] ="*"
+                player_blue[punkt + int(cord[0]) - 2] = "*"
         elif ent == 3:
             placed_blue = placed_blue + 1
             for _ in range(2):
                 punkt += 1
-                player_blue[punkt+int(cord[0]) -2] ="*"
+                player_blue[punkt + int(cord[0]) - 2] = "*"
         elif ent == 4:
             placed_blue = placed_blue + 1
             for _ in range(3):
                 punkt += 1
-                player_blue[punkt+int(cord[0]) -2] ="*"
+                player_blue[punkt + int(cord[0]) - 2] = "*"
         elif ent == 5:
             for _ in range(4):
                 punkt += 1
-                player_blue[punkt+int(cord[0]) -2] ="*"
-    elif int(cord2[1]) - int(cord[1]) == 0 and int(cord2[0]) - int(cord[0]) !=0:
-        ent =abs(int(cord[0]) + int(cord2[0]))
+                player_blue[punkt + int(cord[0]) - 2] = "*"
+    elif int(cord2[1]) - int(cord[1]) == 0 and int(cord2[0]) - int(cord[0]) != 0:
+        ent = abs(int(cord[0]) + int(cord2[0]))
         if cord[0] > cord2[0]:
             punkt = int(cord2[0])
             punkt += -2
-        elif cord2[0]> cord[0]:
+        elif cord2[0] > cord[0]:
             punkt = int(cord[0])
             punkt += -2
         if ent > 5:
@@ -152,40 +157,31 @@ def boot_blue():
             placed_blue = placed_blue + 1
             for _ in range(1):
                 punkt += 1
-                player_blue[punkt+int(cord[1]) *10 -10] ="*"
+                player_blue[punkt + int(cord[1]) * 10 - 10] = "*"
         elif ent == 3:
             placed_blue = placed_blue + 1
             for _ in range(2):
                 punkt += 1
-                player_blue[punkt+int(cord[1]) *10 -10] ="*"
+                player_blue[punkt + int(cord[1]) * 10 - 10] = "*"
         elif ent == 4:
             placed_blue = placed_blue + 1
             for _ in range(3):
                 punkt += 1
-                player_blue[punkt+int(cord[1]) *10 -10] ="*"
+                player_blue[punkt + int(cord[1]) * 10 - 10] = "*"
         elif ent == 5:
             for _ in range(4):
                 punkt += 1
-                player_blue[punkt+int(cord[1]) *10 -10] ="*"
-
+                player_blue[punkt + int(cord[1]) * 10 - 10] = "*"
 
     print_bord_blue()
     boot_blue()
-         
 
-  
 
 boot_blue()
 
 
-
-
-
-
-
-
 def setup_board_blue():
-    print(bcolors.blau + "Blau" + bcolors.end )
+    print(bcolors.blau + "Blau" + bcolors.end)
     print("Prese enter um fortzufahren")
     input()
     clear()
@@ -194,9 +190,6 @@ def setup_board_blue():
         return 0
     else:
         boot_blue()
-
-
-
 
 
 def info():
@@ -220,9 +213,7 @@ def info():
     clear()
     return
 
-
-
-#def setup():
+# def setup():
 #    for rang in range(100):
 #        player_red[rang]         = 0
 #        player_blue[rang]        = 0
@@ -231,12 +222,10 @@ def info():
 #    return
 
 
-
-
-#def main():
+# def main():
 #    setup()
 #    info()
 #    setup_board()
 #    play()
 
-#*/
+# */
